8aa063ab4636dfc25f6932b42ce6b3e75cd4a356
yukaorange <takaoka.webdesigner@gmail.com>
2024-05-26 08:45:48 +0900

    r3fの変更を追加

--- src/hooks/useOrbit.tsx
+++ src/hooks/useOrbit.tsx
@@ -12,8 +12,9 @@ export const useOrbit = (
     origin,
   }: { radius: number; speed: number; origin: THREE.Vector3 }
 ) => {
-  const [prevTime, setPrevTime] = useState(0);
+  const prevTimeRef = useRef(0);
   const scrollData = useScroll();
+  const angleRef = useRef(0);
 
   const degToRad = (deg: number) => (deg * Math.PI) / 180;
 
@@ -21,22 +22,27 @@ export const useOrbit = (
     if (!group.current) return;
 
     const elapsedTime = clock.getElapsedTime();
-    const deltaTime = elapsedTime - prevTime;
+    const deltaTime = elapsedTime - prevTimeRef.current;
 
     const isScrolled = scrollData.offset > 0;
 
     let moveSpeed = isScrolled ? speed * scrollData.offset * 1 : 0;
 
+    
+    const moveAmount = moveSpeed * deltaTime;
+
+    angleRef.current += moveAmount;
+    
     const radX = degToRad(origin.x);
     const radZ = degToRad(origin.z);
 
-    let x = radius * Math.cos(moveSpeed * elapsedTime + radX);
-    let z = radius * Math.sin(moveSpeed * elapsedTime + radZ);
+    let x = radius * Math.cos(angleRef.current + radX);
+    let z = radius * Math.sin(angleRef.current + radZ);
 
     group.current.position.set(x, origin.y, z);
 
     if (isScrolled) {
-      setPrevTime(elapsedTime);
+      prevTimeRef.current = elapsedTime;
     }
   });
 };
